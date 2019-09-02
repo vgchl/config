031c725cc8a5cd766c71679d9ffcb614e6b16c86
Teun van Vegchel <teun.vanvegchel@skyscanner.net>
2019-09-02 16:37:54 +0800

    Config for hyper and vscode

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -53,12 +53,12 @@ module.exports = {
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: false,
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: false,
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: '12px 14px',
